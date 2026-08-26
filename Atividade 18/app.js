ef45879383d2cb78ed07b3f5d97949bc773940d6
Sofia Cardoso <sfcrdsslv@gmail.com>
2026-08-26 14:39:51 -0300

    Add files via upload

---
+++ Atividade 18/app.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let app = express(); //executamdo o express
+
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res){
+    res.send("<html><body>Site da Fatec Sorocaba</body></html>");
+});
+
+app.get('/historia', function(req, res){
+    res.render("secao/historia");
+});
+
+app.get('/cursos', function(req, res){
+    res.render("secao/cursos");
+});
+
+app.get('/professores', function(req, res){
+    res.render("secao/professores");
+});
+
+app.listen(3000, function(){ //o servidor de app.js vai responder na porta 3000
+    console.log("Olá servidor com express foi carregado");
+});
